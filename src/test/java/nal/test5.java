e30b2bd9a1c1d17740792367d8f0ce64cbd7e869
17130467 <44718270+17130467@users.noreply.github.com>
2019-05-15 01:14:13 +0500

    Update test5.java

--- src/test/java/nal/test5.java
+++ src/test/java/nal/test5.java
@@ -6,7 +6,7 @@ import static org.junit.Assert.*;
 /*Аннтоация @Test указывает JUnit, что метод является тестовым методом*/
 import org.junit.Test;
 /*Импортирует из пакета NalogC класс tax*/
-import nalog.nak.tax;
+import nal.tax;
 /*Объявляем новый класс test5*/
 public class test5 {
 	/*Обозначает новую переменную из класса tax*/
